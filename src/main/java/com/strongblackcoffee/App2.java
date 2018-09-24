aff925dbc268d5dab2dfcf5c35b84cca71534dfd
Tim Singletary <tsingle@localhost.localdomain>
2018-09-24 07:45:43 -0400

    added second URL for testing with two security realms

---
+++ src/main/java/com/strongblackcoffee/App2.java
@@ -0,0 +1,11 @@
+package com.strongblackcoffee;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/App2")
+public class App2 extends Application {
+}
